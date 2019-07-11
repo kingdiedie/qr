fd4aa0a31f3ddcb30ffef185e765ccfedffb2661
king <523940687@qq.com>
2019-07-11 08:49:52 +0800

    修改

--- day05/src/main/java/day05/Ljj.java
+++ day05/src/main/java/day05/Ljj.java
@@ -3,5 +3,8 @@ package day05;
 public class Ljj {
 	public static void main(String[] args) {
 		System.out.println("helloWord");
+		System.out.println("helloWord");
+		System.out.println("helloWord");
+		System.out.println("helloWord");
 	}
 }
